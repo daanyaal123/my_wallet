f67d95f4d82f566eafcc0dc7f3405b9b419a5f48
Daanyaal Jameel <daanyaaljameel943@gmail.com>
2022-05-26 01:11:26 +0530

    Added controller for user

--- User-Service/src/main/java/com/example/User/Service/Controller/UserController.java
+++ User-Service/src/main/java/com/example/User/Service/Controller/UserController.java
@@ -1,5 +1,37 @@
 package com.example.User.Service.Controller;
 
+import com.example.User.Service.Repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.User.Service.Entity.User;
+
+@RestController
 public class UserController {
     
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    User addUser (@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users/{id}")
+    Optional<User> findById (@PathVariable int id){
+        return userRepository.findById(id);
+    }
 }
