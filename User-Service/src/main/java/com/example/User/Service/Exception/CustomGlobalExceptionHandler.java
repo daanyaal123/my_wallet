688eb63ad1b253b30cbc40586d91a61d8e893e6b
Daanyaal Jameel <daanyaaljameel943@gmail.com>
2022-05-26 01:19:33 +0530

    added exception class for if id not found

---
+++ User-Service/src/main/java/com/example/User/Service/Exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.User.Service.Exception;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler{
+    
+    @ExceptionHandler(UserNotFound.class)
+    public void springHandleNotFound(HttpServletResponse response) throws IOException{
+        response.sendError(HttpStatus.NOT_FOUND.value());
+    }
+}
---
+++ User-Service/src/main/java/com/example/User/Service/Exception/UserNotFound.java
@@ -0,0 +1,7 @@
+package com.example.User.Service.Exception;
+
+public class UserNotFound extends RuntimeException{
+    public UserNotFound(int id){
+        super("User id not found : " +id);
+    }    
+}
