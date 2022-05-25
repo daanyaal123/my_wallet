6f5630202189d528ad3f8de190941bfd85b5408e
Daanyaal Jameel <daanyaaljameel943@gmail.com>
2022-05-26 01:21:59 +0530

    used exception in controller

--- User-Service/src/main/java/com/example/User/Service/Controller/UserController.java
+++ User-Service/src/main/java/com/example/User/Service/Controller/UserController.java
@@ -3,16 +3,18 @@ package com.example.User.Service.Controller;
 import com.example.User.Service.Repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.example.User.Service.Entity.User;
+import com.example.User.Service.Exception.UserNotFound;
 
 @RestController
 public class UserController {
@@ -26,12 +28,13 @@ public class UserController {
     }
 
     @PostMapping("/users")
+    @ResponseStatus(HttpStatus.CREATED)
     User addUser (@RequestBody User newUser){
         return userRepository.save(newUser);
     }
 
     @GetMapping("/users/{id}")
-    Optional<User> findById (@PathVariable int id){
-        return userRepository.findById(id);
+    User findById (@PathVariable int id){
+        return userRepository.findById(id).orElseThrow(()-> new UserNotFound(id));
     }
 }
